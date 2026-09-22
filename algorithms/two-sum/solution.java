d74779549329c2b4b8a4708f9d182ecf2c5e53c9
Himanshu Kumar <himanshuranjan1981@gmail.com>
2026-09-22 23:04:01 +0530

    feat: add Two Sum solution

--- algorithms/two-sum/solution.java
+++ algorithms/two-sum/solution.java
@@ -1,6 +1,6 @@
-import java.util.*;class Solution {    public int[] twoSum(int[] nums, int target) {        HashMap<Integer, Integer> map = new HashMap<>();        for (int i = 0; i < nums.length; i++) {            int need = target - nums[i];            if (map.containsKey(need)) {                return new int[]{map.get(need), i};            }            map.put(nums[i], i);        }        return new int[]{};    }}
+import java.util.*;// checkig for duplicationclass Solution {    public int[] twoSum(int[] nums, int target) {        HashMap<Integer, Integer> map = new HashMap<>();        for (int i = 0; i < nums.length; i++) {            int need = target - nums[i];            if (map.containsKey(need)) {                return new int[]{map.get(need), i};            }            map.put(nums[i], i);        }        return new int[]{};    }}
 import java.util.*;
-
+// checkig for duplication
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> map = new HashMap<>();
