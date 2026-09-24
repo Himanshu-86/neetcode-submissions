668695de6bc0d11854a056fbcf27fca4febd1ef8
Himanshu Kumar <himanshuranjan1981@gmail.com>
2026-09-24 09:35:40 +0530

    Add: course-schedule - submission-0

---
+++ Data Structures & Algorithms/course-schedule/submission-0.java
@@ -0,0 +1,52 @@
+class Solution{
+    ArrayList<Integer>[] graph;
+    int[] state;
+
+    public boolean canFinish(int numCourses,int[][] prerequisites){
+        graph=new ArrayList[numCourses];
+        state=new int[numCourses];
+
+        for(int i=0;i<numCourses;i++){
+            graph[i]=new ArrayList<>();
+        }
+
+        for(int[] p:prerequisites){
+            int course=p[0];
+            int prerequisite=p[1];
+
+            graph[prerequisite].add(course);
+        }
+
+        for(int i=0;i<numCourses;i++){
+            if(state[i]==0){
+                if(!dfs(i)){
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    boolean dfs(int node){
+        if(state[node]==1){
+            return false;
+        }
+
+        if(state[node]==2){
+            return true;
+        }
+
+        state[node]=1;
+
+        for(int neighbor:graph[node]){
+            if(!dfs(neighbor)){
+                return false;
+            }
+        }
+
+        state[node]=2;
+
+        return true;
+    }
+}
